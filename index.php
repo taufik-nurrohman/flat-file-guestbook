f0cfd0716cfe6249a16bd9c5164e2764c1548968
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-05-31 11:23:52 +0700

    Avoid Confirmation Dialog on Manual Refresh
    After a message successfully sent.

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Flat-File Guestbook</title>
-    <link href="style.css" rel="stylesheet">
-  </head>
-  <body>
+<?php session_start();
 
-    <div class="guestbook" id="guestbook">
-      <div class="guest-messages" id="guest-messages">
-<?php
-
-// ===================================================================
-// PHP FLAT-FILE GUESTBOOK by Taufik Nurrohman <http://gplus.to/tovic>
-// ===================================================================
+// =================================================================================
+// PHP FLAT-FILE GUESTBOOK by Taufik Nurrohman <https://github.com/taufik-nurrohman>
+// =================================================================================
 
 // START CONFIGURATION
 $database = 'database-001'; // Your TXT file name as database.
@@ -24,7 +12,6 @@ $max_length_name = 60; // Maximum character length for guest name
 $max_length_url = 120; // Maximum character length for guest URL
 $max_length_message = 1000; // Maximum character length for guest message
 $messages = array(
-    'database_missing' => 'Database not found. Created one. Please reload the page.',
     'name_missing' => 'Please enter your name.',
     'url_invalid' => 'Invalid URL.',
     'message_missing' => 'Please enter your message.',
@@ -46,8 +33,8 @@ function create_or_update_file($file_path, $data) {
     fwrite($handle, $data);
 }
 
-// Filter HTML outputs. 
-// The rest will appear as plain HTML entities to prevent XSS. 
+// Filter HTML outputs.
+// The rest will appear as plain HTML entities to prevent XSS.
 // => http://en.wikipedia.org/wiki/Cross-site_scripting
 function filter_html($data) {
     return preg_replace(
@@ -68,29 +55,27 @@ function filter_html($data) {
 
 // Redefine database name via URL to load
 // Load database-002.txt => http://localhost/guestbook/index.php&data=database-002
-if(isset($_GET['data'])) {
-    $database = $_GET['data'];
+if (isset($_GET['data'])) {
+    $database = basename($_GET['data']);
 }
 
+$error = ""; // error messages
+
 // Check whether the "database" is not available. If not, create one!
-if( ! file_exists($database . '.txt')) {
+if (!file_exists($database . '.txt')) {
     // Prevent guest to create new database via `data=database-XXX` in URL
     // Only administrator can do this by editing the `$database` value
-    if( ! isset($_GET['data'])) {
+    if (!isset($_GET['data'])) {
         create_or_update_file($database . '.txt', "");
-        echo "<p class=\"message-warning\">" . $messages['database_missing'] . "</p>";
     }
-    return false;
 } else {
     $old_data = file_get_contents($database . '.txt');
 }
 
-
 /**
  * Post a message
  */
-$error = ""; // error messages
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $name = "";
     $url = "";
@@ -98,15 +83,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $timestamp = date('U');
 
     // Make sure the guest name is not empty.
-    if(isset($_POST['name']) && ! empty($_POST['name'])) {
+    if (isset($_POST['name']) && !empty($_POST['name'])) {
         $name = strip_tags($_POST['name']);
     } else {
         $error .= "<p class=\"message-error\">" . $messages['name_missing'] . "</p>";
     }
 
-    // Make sure the URL format is valid. Set its value as `-` if empty. 
-    if(isset($_POST['url']) && ! empty($_POST['url'])) {
-        if(filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
+    // Make sure the URL format is valid. Set its value as `-` if empty.
+    if (isset($_POST['url']) && !empty($_POST['url'])) {
+        if (filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
             $url = strip_tags($_POST['url']);
         } else {
             $error .= "<p class=\"message-error\">" . $messages['url_invalid'] . "</p>";
@@ -116,7 +101,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     // Make sure the guest message is not empty.
-    if(isset($_POST['message']) && ! empty($_POST['message'])) {
+    if (isset($_POST['message']) && !empty($_POST['message'])) {
         $message = preg_replace(
             array(
                 '/[\n\r]{4,}/', // [1]
@@ -135,56 +120,58 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                 '$6' // Unlink all links in message content!
             ),
         $_POST['message']);
-        $message = htmlentities($message, ENT_QUOTES, 'UTF-8'); // [2]
+        $message = htmlspecialchars($message, ENT_QUOTES); // [2]
     } else {
         $error .= "<p class=\"message-error\">" . $messages['message_missing'] . "</p>";
     }
 
     // Check the math challenge answer to prevent spam robot.
-    if( ! isset($_POST['math']) || empty($_POST['math']) || $_POST['math'] != $_SESSION['math']) {
+    if (!isset($_POST['math']) || empty($_POST['math']) || $_POST['math'] != $_SESSION['math']) {
         $error .= "<p class=\"message-error\">" . $messages['math_invalid'] . "</p>";
     }
 
     // Check for character length limit
-    if(strlen($name) > $max_length_name) $error .= "<p class=\"message-error\">" . $messages['max_length_name'] . "</p>";
-    if(strlen($url) > $max_length_url) $error .= "<p class=\"message-error\">" . $messages['max_length_url'] . "</p>";
-    if(strlen($message) > $max_length_message) $error .= "<p class=\"message-error\">" . $messages['max_length_message'] . "</p>";
+    if (strlen($name) > $max_length_name) $error .= "<p class=\"message-error\">" . $messages['max_length_name'] . "</p>";
+    if (strlen($url) > $max_length_url) $error .= "<p class=\"message-error\">" . $messages['max_length_url'] . "</p>";
+    if (strlen($message) > $max_length_message) $error .= "<p class=\"message-error\">" . $messages['max_length_message'] . "</p>";
+
+    // [3]
+    $_SESSION['guest_name'] = isset($_POST['name']) ? $_POST['name'] : "";
+    $_SESSION['guest_url'] = isset($_POST['url']) ? $_POST['url'] : "";
+    $_SESSION['guest_message'] = isset($_POST['message']) && $error != "" ? htmlspecialchars($_POST['message'], ENT_QUOTES) : "";
 
     // If all data entered by guest is valid, insert new data!
-    if($error === "") {
+    if (!$error) {
         $new_data = $name . "\n" . $url . "\n" . $message . "\n" . $timestamp;
-        if( ! empty($old_data)) {
+        if (!empty($old_data)) {
             create_or_update_file($database . '.txt', $new_data . "\n\n==\n" . $old_data); // Prepend data
         } else {
             create_or_update_file($database . '.txt', $new_data); // Insert data
         }
+        $hash = '#guest-' . $timestamp;
     } else {
-        // else, print the error messages.
-        echo $error;
+        $_SESSION['error'] = $error;
+        $hash = "";
     }
+
+    header('location: ' . basename(__FILE__) . $hash);
+    exit;
 
 }
 
-// [3]
-$_SESSION['guest_name'] = isset($_POST['name']) ? $_POST['name'] : "";
-$_SESSION['guest_url'] = isset($_POST['url']) ? $_POST['url'] : "http://";
-$_SESSION['guest_message'] = isset($_POST['message']) && $error != "" ? htmlentities($_POST['message'], ENT_QUOTES, 'UTF-8') : "";
+$error = isset($_SESSION['error']) ? $_SESSION['error'] : "";
+$guest_name = isset($_SESSION['guest_name']) ? $_SESSION['guest_name'] : "";
+$guest_url = isset($_SESSION['guest_url']) ? $_SESSION['guest_url'] : "";
+$guest_message = isset($_SESSION['guest_message']) ? $_SESSION['guest_message'] : "";
 
-// ----------------------------------------------------------------------------------------
-// [1]. Prevent guest to type too many line break symbols.
-// People usually do these thing to make their SPAM messages looks striking.
-// [2]. Convert all HTML tags into HTML entities. This is done thoroughly for safety.
-// We can revert back the escaped HTML into normal HTML tags later via `filter_html()`
-// [3]. Save the form data into session. So if something goes wrong, the data entered
-// by guest will still be stored in the form after submitting.
-// ----------------------------------------------------------------------------------------
+// Clear sessions
+unset($_SESSION['error'], $_SESSION['guest_name'], $_SESSION['guest_url'], $_SESSION['guest_message']);
 
-
-// Math challenge to prevent spam robot. 
+// Math challenge to prevent spam robot.
 // Current answer will be stored in `$_SESSION['math']`
 $x = mt_rand(1, 10);
 $y = mt_rand(1, 10);
-if($x - $y > 0) {
+if ($x - $y > 0) {
     $math = $x . ' - ' . $y;
     $_SESSION['math'] = $x - $y;
 } else {
@@ -195,6 +182,14 @@ if($x - $y > 0) {
 // Testing...
 // echo $math . ' = ' . $_SESSION['math'];
 
+// ----------------------------------------------------------------------------------------
+// [1]. Prevent guest to type too many line break symbols.
+// People usually do these thing to make their SPAM messages looks striking.
+// [2]. Convert all HTML tags into HTML entities. This is done thoroughly for safety.
+// We can revert back the escaped HTML into normal HTML tags later via `filter_html()`
+// [3]. Save the form data into session. So if something goes wrong, the data entered
+// by guest will still be stored in the form after submitting.
+// ----------------------------------------------------------------------------------------
 
 /**
  * Show the existing data.
@@ -203,15 +198,28 @@ $data = file_get_contents($database . '.txt');
 $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
 $nav = "";
 
-if( ! empty($data)) {
+echo "<!DOCTYPE html>\n";
+echo "<html dir=\"ltr\">\n";
+echo "  <head>\n";
+echo "    <meta charset=\"utf-8\">\n";
+echo "    <title>Flat-File Guestbook</title>\n";
+echo "    <link href=\"index.css\" rel=\"stylesheet\">\n";
+echo "  </head>\n";
+echo "  <body>\n";
+echo "    <div class=\"guestbook\" id=\"guestbook\">\n";
+echo "      <div class=\"guest-messages\" id=\"guest-messages\">\n";
+
+echo $error ? "        " . $error . "\n" : "";
+
+if (!empty($data)) {
 
     $data = explode("\n\n==\n", $data);
     $total_pages = ceil(count($data) / $per_page);
 
     // Create navigation if the number of pages is more than 1.
-    if($total_pages > 1) {
-        for($i = 0; $i < $total_pages; $i++) {
-            if($current_page == ($i + 1)) {
+    if ($total_pages > 1) {
+        for ($i = 0; $i < $total_pages; $i++) {
+            if ($current_page == ($i + 1)) {
                 $nav .= " <span>" . ($i + 1) . "</span>"; // Disabled navigation
             } else {
                 $nav .= " <a href=\"?page=" . ($i + 1) . (isset($_GET['data']) ? '&amp;data=' . $database : '') . "\">" . ($i + 1) . "</a>";
@@ -219,12 +227,12 @@ if( ! empty($data)) {
         }
     }
 
-    for($i = 0; $i < count($data); $i++) {
+    for ($i = 0; $i < count($data); $i++) {
         $item = explode("\n", $data[$i]);
         // Permalink (single item)
         // http://localhost/guestbook/index.php&data=database-001&guest=0123456789
-        if(isset($_GET['guest']) && preg_match('/[0-9]+/', $_GET['guest'])) {
-            if($item[3] == $_GET['guest']) {
+        if (isset($_GET['guest']) && preg_match('/[0-9]+/', $_GET['guest'])) {
+            if ($item[3] == $_GET['guest']) {
                 echo "        <div class=\"guest-item\" id=\"guest-" . $item[3] . "\">\n";
                 echo "          <strong class=\"guest-name\">";
                 echo $item[1] == "-" ? "" : "<a href=\"" . $item[1] . "\" rel=\"nofollow\" target=\"_blank\">";
@@ -240,7 +248,7 @@ if( ! empty($data)) {
             }
         // Normal list
         } else {
-            if($i <= ($per_page * $current_page) - 1 && $i > ($per_page * ($current_page - 1)) - 1) {
+            if ($i <= ($per_page * $current_page) - 1 && $i > ($per_page * ($current_page - 1)) - 1) {
                 echo "        <div class=\"guest-item\" id=\"guest-" . $item[3] . "\">\n";
                 echo "          <strong class=\"guest-name\">";
                 echo $item[1] == "-" ? "" : "<a href=\"" . $item[1] . "\" rel=\"nofollow\" target=\"_blank\">";
@@ -264,23 +272,24 @@ if( ! empty($data)) {
     echo "        </div>\n";
 }
 
-?>
-      </div>
-      <div class="guestbook-nav"><?php echo trim($nav); ?></div>
-<?php if( ! isset($_GET['data']) && ! isset($_GET['guest'])): ?>
-      <form method="post">
-        <label>Name</label>
-        <div><input type="text" name="name" value="<?php echo isset($_SESSION['guest_name']) ? $_SESSION['guest_name'] : ''; ?>"></div>
-        <label>URL</label>
-        <div><input type="url" name="url" value="<?php echo isset($_SESSION['guest_url']) ? $_SESSION['guest_url'] : 'http://'; ?>"></div>
-        <label>Message</label>
-        <div><textarea name="message"><?php echo isset($_SESSION['guest_message']) ? $_SESSION['guest_message'] : ''; ?></textarea></div>
-        <hr>
-        <div><?php echo $math; ?> = <input type="text" name="math" autocomplete="off"> <button type="submit">Send Message</button></div>
-        <span class="clear"></span>
-      </form>
-<?php endif; ?>
-    </div>
+if (!isset($_GET['data']) && !isset($_GET['guest'])) {
+    echo "      </div>\n";
+    echo "      <div class=\"guestbook-nav\">" . trim($nav) . "</div>\n";
+    echo "      <form method=\"post\">\n";
+    echo "        <label>Name</label>\n";
+    echo "        <div><input type=\"text\" name=\"name\" value=\"" . htmlspecialchars($guest_name) . "\"></div>\n";
+    echo "        <label>URL</label>\n";
+    echo "        <div><input type=\"url\" name=\"url\" placeholder=\"http://" . $_SERVER['HTTP_HOST'] . "\" value=\"" . htmlspecialchars($guest_url) . "\"></div>\n";
+    echo "        <label>Message</label>\n";
+    echo "        <div><textarea name=\"message\">" . htmlspecialchars($guest_message) . "</textarea></div>\n";
+    echo "        <hr>\n";
+    echo "        <div>" . $math . " = <input type=\"text\" name=\"math\" autocomplete=\"off\"> <button type=\"submit\">Send Message</button></div>\n";
+    echo "        <span class=\"clear\"></span>\n";
+    echo "      </form>\n";
+} else {
+    echo "      </div>\n";
+}
 
-  </body>
-</html>
+echo "    </div>\n";
+echo "  </body>\n";
+echo "</html>";
